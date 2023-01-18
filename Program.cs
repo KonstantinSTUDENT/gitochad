54fa6a293bcd6053d7d0e093e06c0e881adce73a
KonstantinSTYDENT <Karpovichprivat@gmail.com>
2023-01-18 22:17:57 +0300

    первая моя программа

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число:  ");
+int number = int.Parse(Console.ReadLine ()!);
+int num = number * number;
+Console.WriteLine(num);
+Console.WriteLine($"Квадрат числа {number} равен {num}");
